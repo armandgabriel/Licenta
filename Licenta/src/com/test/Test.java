0585c65fcfe9b443a7a1c6798373743b1a0304c0
Camner Armand Gabriel <armand.camner@ro.ibm.com>
2018-11-04 23:35:47 +0200

    Another test to push

--- Licenta/src/com/test/Test.java
+++ Licenta/src/com/test/Test.java
@@ -8,6 +8,7 @@ public class Test {
 		System.out.println("Git Parent Directory: C:\\Users\\IBM_ADMIN\\git\\repository");
 		System.out.println("Hei Man!");
 		System.out.println("Test1");
+		System.out.println("Added a new text file!");
 	}
 
 }
