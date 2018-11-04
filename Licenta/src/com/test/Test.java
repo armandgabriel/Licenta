5b13fb60cd560a266cf78145b4fcdfa4e625368c
Camner Armand Gabriel <armand.camner@ro.ibm.com>
2018-11-04 22:52:29 +0200

    Project Commit Version 1 Just a simple "Hello World" file added.
    This project will be my project for final exam of collage.
    
    Armand

---
+++ Licenta/src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("Git Parent Directory: C:\\Users\\IBM_ADMIN\\git\\repository");
+		System.out.println("Hei Man!");
+	}
+
+}
