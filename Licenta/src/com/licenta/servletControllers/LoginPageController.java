2d88b8a3342083f58ecf01bfa0fb62f9eee25c71
User <User@User-PC>
2018-11-13 00:34:35 -0800

    Aded new cap

---
+++ Licenta/src/com/licenta/servletControllers/LoginPageController.java
@@ -0,0 +1,78 @@
+package com.licenta.servletControllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serializable;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginPageController
+ */
+public class LoginPageController extends HttpServlet implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginPageController() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletInfo()
+	 */
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null; 
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		 try {
+	         out.println("<!DOCTYPE html>");
+	         out.println("<html><head>");
+	         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+	         out.println("<title>Hello, World</title></head>");
+	         out.println("<body>");
+	         out.println("<h1>Hello, world!</h1>");  // says Hello
+	         // Echo client's request information
+	         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+	         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+	         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+	         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+	         // Generate a random number upon each request
+	         out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+	         out.println("</body>");
+	         out.println("</html>");
+	      } finally {
+	         out.close();  // Always close the output writer
+	      }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
